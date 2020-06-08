d3cdce3e89f47dcf642043badbdc2baedb5b4748
Kamila Duda <kakus86@o2.pl>
2020-06-08 22:20:36 +0200

    mój plan diety

---
+++ script.js
@@ -0,0 +1,26 @@
+let listItems = document.querySelectorAll(".listItem");
+
+listItems.forEach(function (listItem) {
+    listItem.addEventListener("click", (ev) => {
+        ev.target.classList.toggle("checkedListItem");
+    });
+});
+
+
+
+let doneLists = document.querySelectorAll("input");
+doneLists.forEach(function (doneList) {
+    doneList.addEventListener("click", () => {
+       let checkId = doneList.getAttribute('id');
+
+        if(checkId==="first"){
+            document.getElementById("firstList").classList.toggle("notDisplay");
+        }else if(checkId==="second"){
+            document.getElementById("secondList").classList.toggle("notDisplay");
+        }else{
+            document.getElementById("thirdList").classList.toggle("notDisplay");
+        };
+        
+    });
+    
+});
